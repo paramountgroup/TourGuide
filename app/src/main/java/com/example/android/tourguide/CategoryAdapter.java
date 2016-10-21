43ca5c817fbceb6f1170edb612aa8bf33ee4b48b
Bob Ingram <bob@theparamountgroup.us>
2016-10-21 09:06:34 -0400

    Save prior to changes audio link to a URL intent

--- app/src/main/java/com/example/android/tourguide/CategoryAdapter.java
+++ app/src/main/java/com/example/android/tourguide/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.tourguide;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,15 +11,20 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
+    /** Context of the app */
+    private Context mContext;
+
     /**
      * Create a new {@link CategoryAdapter} object.
      *
      * @param fm is the fragment manager that will keep each fragment's state in the adapter
      *           across swipes.
      */
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
+
 
     /**
      * Return the {@link Fragment} that should be displayed for the given page number.
@@ -43,4 +49,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return 4;
     }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.parks);
+        } else if (position == 1) {
+            return mContext.getString(R.string.entertainment);
+        } else if (position == 2) {
+            return mContext.getString(R.string.restaurants);
+        } else {
+            return mContext.getString(R.string.beaches);
+        }
+    }
+
+
 }
